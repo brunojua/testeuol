71d606ae651232251e994b7ccd276ee59c02c44a
Bruno <brunog1jua@gmail.com>
2021-04-18 22:23:11 -0300

    Configurado app component ts

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './core/auth/auth.guard';
+import { LoginComponent } from './login/login.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'github', loadChildren: () => import('./github/github.module').then(m => m.GithubModule), canActivate: [AuthGuard]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { AuthService } from './core/auth/auth.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'front';
+  title = 'app';
+  route: String;
+  constructor(private location: Location, private router: Router, private auth: AuthService, private activatedRouter: ActivatedRoute) {
+    this.router.events.subscribe((ev) => {
+
+      if (ev instanceof NavigationEnd) {
+        console.log(this.location.path())
+        if (this.location.path() == '' || this.location.path() == '/') {
+          if (this.auth.isAuthenticated()) {
+            this.router.navigate(['github'])
+          } else {
+            this.router.navigate(['login'])
+          }
+        }
+      }
+    });
+  }
+
+  ngOnInit(): void {
+  }
 }
